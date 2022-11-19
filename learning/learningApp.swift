e92793fd74b1bc7e6b80fbef29be58ed725f531a
orlandev <onovas05@gmail.com>
2022-11-19 10:13:05 -0500

    Initial Commit

---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by OrlanDev on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
